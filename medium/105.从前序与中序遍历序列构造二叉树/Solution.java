1ccf8b94b99b8ed3c8e62546e123f218e2880c5b
xiyusullos <i@xy-jit.cc>
2020-05-22 23:50:30 +0800

    借鉴: medium/105.从前序与中序遍历序列构造二叉树

---
+++ medium/105.从前序与中序遍历序列构造二叉树/Solution.java
@@ -0,0 +1,95 @@
+/*
+ * @Author: aponder
+ * @Date: 2020-05-22 23:37:00
+ * @LastEditTime: 2020-05-22 23:50:19
+ * @LastEditors: aponder
+ * @Description: 
+ * @FilePath: /leetcode-zh/medium/105.从前序与中序遍历序列构造二叉树/Solution.java
+ */ 
+/*
+ * @lc app=leetcode.cn id=105 lang=java
+ *
+ * [105] 从前序与中序遍历序列构造二叉树
+ *
+ * https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (64.73%)
+ * Likes:    496
+ * Dislikes: 0
+ * Total Accepted:    79.7K
+ * Total Submissions: 119K
+ * Testcase Example:  '[3,9,20,15,7]\n[9,3,15,20,7]'
+ *
+ * 根据一棵树的前序遍历与中序遍历构造二叉树。
+ * 
+ * 注意:
+ * 你可以假设树中没有重复的元素。
+ * 
+ * 例如，给出
+ * 
+ * 前序遍历 preorder = [3,9,20,15,7]
+ * 中序遍历 inorder = [9,3,15,20,7]
+ * 
+ * 返回如下的二叉树：
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private Map<Integer, Integer> indexMap;
+
+    public TreeNode myBuildTree(int[] preorder, int[] inorder, int preorder_left, int preorder_right, int inorder_left, int inorder_right) {
+        if (preorder_left > preorder_right) {
+            return null;
+        }
+
+        // 前序遍历中的第一个节点就是根节点
+        int preorder_root = preorder_left;
+        // 在中序遍历中定位根节点
+        int inorder_root = indexMap.get(preorder[preorder_root]);
+        
+        // 先把根节点建立出来
+        TreeNode root = new TreeNode(preorder[preorder_root]);
+        // 得到左子树中的节点数目
+        int size_left_subtree = inorder_root - inorder_left;
+        // 递归地构造左子树，并连接到根节点
+        // 先序遍历中「从 左边界+1 开始的 size_left_subtree」个元素就对应了中序遍历中「从 左边界 开始到 根节点定位-1」的元素
+        root.left = myBuildTree(preorder, inorder, preorder_left + 1, preorder_left + size_left_subtree, inorder_left, inorder_root - 1);
+        // 递归地构造右子树，并连接到根节点
+        // 先序遍历中「从 左边界+1+左子树节点数目 开始到 右边界」的元素就对应了中序遍历中「从 根节点定位+1 到 右边界」的元素
+        root.right = myBuildTree(preorder, inorder, preorder_left + size_left_subtree + 1, preorder_right, inorder_root + 1, inorder_right);
+        return root;
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = preorder.length;
+        // 构造哈希映射，帮助我们快速定位根节点
+        indexMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < n; i++) {
+            indexMap.put(inorder[i], i);
+        }
+        return myBuildTree(preorder, inorder, 0, n - 1, 0, n - 1);
+    }
+}
+// @lc code=end
+
+// Accepted
+// 203/203 cases passed (3 ms)
+// Your runtime beats 78.73 % of java submissions
+// Your memory usage beats 80 % of java submissions (39.5 MB)
