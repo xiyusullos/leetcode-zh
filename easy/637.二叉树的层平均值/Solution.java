d06eb482ceddf8a637907ca537a62c2acb4955b6
xiyusullos <i@xy-jit.cc>
2020-05-29 23:56:33 +0800

    借鉴: easy/637.二叉树的层平均值

---
+++ easy/637.二叉树的层平均值/Solution.java
@@ -0,0 +1,85 @@
+/*
+ * @Author: aponder
+ * @Date: 2020-05-29 23:55:29
+ * @LastEditTime: 2020-05-29 23:56:23
+ * @LastEditors: aponder
+ * @Description: 
+ * @FilePath: /leetcode-zh/easy/637.二叉树的层平均值/Solution.java
+ */ 
+/*
+ * @lc app=leetcode.cn id=637 lang=java
+ *
+ * [637] 二叉树的层平均值
+ *
+ * https://leetcode-cn.com/problems/average-of-levels-in-binary-tree/description/
+ *
+ * algorithms
+ * Easy (63.76%)
+ * Likes:    118
+ * Dislikes: 0
+ * Total Accepted:    19.5K
+ * Total Submissions: 30.2K
+ * Testcase Example:  '[3,9,20,15,7]'
+ *
+ * 给定一个非空二叉树, 返回一个由每层节点平均值组成的数组.
+ * 
+ * 示例 1:
+ * 
+ * 输入:
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 输出: [3, 14.5, 11]
+ * 解释:
+ * 第0层的平均值是 3,  第1层是 14.5, 第2层是 11. 因此返回 [3, 14.5, 11].
+ * 
+ * 
+ * 注意：
+ * 
+ * 
+ * 节点值的范围在32位有符号整数范围内。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List < Double > res = new ArrayList < > ();
+        Queue < TreeNode > queue = new LinkedList < > ();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            long sum = 0, count = 0;
+            Queue < TreeNode > temp = new LinkedList < > ();
+            while (!queue.isEmpty()) {
+                TreeNode n = queue.remove();
+                sum += n.val;
+                count++;
+                if (n.left != null)
+                    temp.add(n.left);
+                if (n.right != null)
+                    temp.add(n.right);
+            }
+            queue = temp;
+            res.add(sum * 1.0 / count);
+        }
+        return res;
+    }
+}
+// @lc code=end
+
+// Accepted
+// 65/65 cases passed (3 ms)
+// Your runtime beats 75.64 % of java submissions
+// Your memory usage beats 26.67 % of java submissions (41.6 MB)
