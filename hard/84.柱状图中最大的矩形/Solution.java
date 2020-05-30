6d65c43a838da513f61fc0afceed49a7d22b3b16
xiyusullos <i@xy-jit.cc>
2020-05-30 23:30:58 +0800

    借鉴: hard/84.柱状图中最大的矩形

---
+++ hard/84.柱状图中最大的矩形/Solution.java
@@ -0,0 +1,83 @@
+/*
+ * @Author: aponder
+ * @Date: 2020-05-30 23:30:10
+ * @LastEditTime: 2020-05-30 23:30:43
+ * @LastEditors: aponder
+ * @Description: 
+ * @FilePath: /leetcode-zh/hard/84.柱状图中最大的矩形/Solution.java
+ */ 
+/*
+ * @lc app=leetcode.cn id=84 lang=java
+ *
+ * [84] 柱状图中最大的矩形
+ *
+ * https://leetcode-cn.com/problems/largest-rectangle-in-histogram/description/
+ *
+ * algorithms
+ * Hard (39.26%)
+ * Likes:    706
+ * Dislikes: 0
+ * Total Accepted:    59.8K
+ * Total Submissions: 147.2K
+ * Testcase Example:  '[2,1,5,6,2,3]'
+ *
+ * 给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+ * 
+ * 求在该柱状图中，能够勾勒出来的矩形的最大面积。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 以上是柱状图的示例，其中每个柱子的宽度为 1，给定的高度为 [2,1,5,6,2,3]。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 图中阴影部分为所能勾勒出的最大矩形面积，其面积为 10 个单位。
+ * 
+ * 
+ * 
+ * 示例:
+ * 
+ * 输入: [2,1,5,6,2,3]
+ * 输出: 10
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    // public int largestRectangleArea(int[] heights) {
+
+    // }
+
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Arrays.fill(right, n);
+        
+        Stack<Integer> mono_stack = new Stack<Integer>();
+        for (int i = 0; i < n; ++i) {
+            while (!mono_stack.isEmpty() && heights[mono_stack.peek()] >= heights[i]) {
+                right[mono_stack.peek()] = i;
+                mono_stack.pop();
+            }
+            left[i] = (mono_stack.isEmpty() ? -1 : mono_stack.peek());
+            mono_stack.push(i);
+        }
+        
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
+// Accepted
+// 96/96 cases passed (11 ms)
+// Your runtime beats 90.11 % of java submissions
+// Your memory usage beats 43.48 % of java submissions (41.1 MB)
