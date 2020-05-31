56e88be5ba897fea7dab05d16e0d56bf6b2e376c
xiyusullos <i@xy-jit.cc>
2020-05-31 23:26:21 +0800

    借鉴: easy/653.两数之和-iv-输入-bst

---
+++ easy/653.两数之和-iv-输入-bst/Solution.java
@@ -0,0 +1,95 @@
+/*
+ * @Author: aponder
+ * @Date: 2020-05-31 23:20:16
+ * @LastEditTime: 2020-05-31 23:26:06
+ * @LastEditors: aponder
+ * @Description: 
+ * @FilePath: /leetcode-zh/easy/653.两数之和-iv-输入-bst/Solution.java
+ */ 
+/*
+ * @lc app=leetcode.cn id=653 lang=java
+ *
+ * [653] 两数之和 IV - 输入 BST
+ *
+ * https://leetcode-cn.com/problems/two-sum-iv-input-is-a-bst/description/
+ *
+ * algorithms
+ * Easy (54.63%)
+ * Likes:    137
+ * Dislikes: 0
+ * Total Accepted:    15.9K
+ * Total Submissions: 28.9K
+ * Testcase Example:  '[5,3,6,2,4,null,7]\n9'
+ *
+ * 给定一个二叉搜索树和一个目标结果，如果 BST 中存在两个元素且它们的和等于给定的目标结果，则返回 true。
+ * 
+ * 案例 1:
+ * 
+ * 
+ * 输入: 
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 3   6
+ * ⁠/ \   \
+ * 2   4   7
+ * 
+ * Target = 9
+ * 
+ * 输出: True
+ * 
+ * 
+ * 
+ * 
+ * 案例 2:
+ * 
+ * 
+ * 输入: 
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 3   6
+ * ⁠/ \   \
+ * 2   4   7
+ * 
+ * Target = 28
+ * 
+ * 输出: False
+ * 
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    // public boolean findTarget(TreeNode root, int k) {
+        
+    // }
+
+    public boolean findTarget(TreeNode root, int k) {
+        Set < Integer > set = new HashSet();
+        return find(root, k, set);
+    }
+    public boolean find(TreeNode root, int k, Set < Integer > set) {
+        if (root == null)
+            return false;
+        if (set.contains(k - root.val))
+            return true;
+        set.add(root.val);
+        return find(root.left, k, set) || find(root.right, k, set);
+    }
+}
+// @lc code=end
+
+// Accepted
+// 421/421 cases passed (4 ms)
+// Your runtime beats 57.67 % of java submissions
+// Your memory usage beats 87.5 % of java submissions (40.6 MB)
