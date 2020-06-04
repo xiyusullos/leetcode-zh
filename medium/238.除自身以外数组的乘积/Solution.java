edcf6ff9923f53ee65417321142b5dd82d409d17
xiyusullos <i@xy-jit.cc>
2020-06-04 21:23:16 +0800

    medium/238.除自身以外数组的乘积

--- medium/238.除自身以外数组的乘积/Solution.java
+++ medium/238.除自身以外数组的乘积/Solution.java
@@ -1,7 +1,7 @@
 /*
  * @Author: aponder
  * @Date: 2020-06-04 21:16:31
- * @LastEditTime: 2020-06-04 21:17:35
+ * @LastEditTime: 2020-06-04 21:23:11
  * @LastEditors: aponder
  * @Description: 
  * @FilePath: /leetcode-zh/medium/238.除自身以外数组的乘积/Solution.java
@@ -44,42 +44,60 @@
 
 // @lc code=start
 class Solution {
-    // public int[] productExceptSelf(int[] nums) {
-
-    // }
-
     public int[] productExceptSelf(int[] nums) {
-        int length = nums.length;
-
-        // L 和 R 分别表示左右两侧的乘积列表
-        int[] L = new int[length];
-        int[] R = new int[length];
-
-        int[] answer = new int[length];
-
-        // L[i] 为索引 i 左侧所有元素的乘积
-        // 对于索引为 '0' 的元素，因为左侧没有元素，所以 L[0] = 1
-        L[0] = 1;
-        for (int i = 1; i < length; i++) {
-            L[i] = nums[i - 1] * L[i - 1];
+        int left = 1, right = 1;
+        int l = nums.length;
+        int[] result = new int[l];
+        for (int i = 0; i < l; i++) {
+            result[i] = 1;
         }
 
-        // R[i] 为索引 i 右侧所有元素的乘积
-        // 对于索引为 'length-1' 的元素，因为右侧没有元素，所以 R[length-1] = 1
-        R[length - 1] = 1;
-        for (int i = length - 2; i >= 0; i--) {
-            R[i] = nums[i + 1] * R[i + 1];
+        for (int i = 0; i < l; i++) {
+            result[i] *= left;
+            result[l - 1 - i] *= right;
+            left *= nums[i];
+            right *= nums[l - 1 - i];
         }
-
-        // 对于索引 i，除 nums[i] 之外其余各元素的乘积就是左侧所有元素的乘积乘以右侧所有元素的乘积
-        for (int i = 0; i < length; i++) {
-            answer[i] = L[i] * R[i];
-        }
-
-        return answer;
+        return result;
     }
+
+    // public int[] productExceptSelf(int[] nums) {
+    //     int length = nums.length;
+
+    //     // L 和 R 分别表示左右两侧的乘积列表
+    //     int[] L = new int[length];
+    //     int[] R = new int[length];
+
+    //     int[] answer = new int[length];
+
+    //     // L[i] 为索引 i 左侧所有元素的乘积
+    //     // 对于索引为 '0' 的元素，因为左侧没有元素，所以 L[0] = 1
+    //     L[0] = 1;
+    //     for (int i = 1; i < length; i++) {
+    //         L[i] = nums[i - 1] * L[i - 1];
+    //     }
+
+    //     // R[i] 为索引 i 右侧所有元素的乘积
+    //     // 对于索引为 'length-1' 的元素，因为右侧没有元素，所以 R[length-1] = 1
+    //     R[length - 1] = 1;
+    //     for (int i = length - 2; i >= 0; i--) {
+    //         R[i] = nums[i + 1] * R[i + 1];
+    //     }
+
+    //     // 对于索引 i，除 nums[i] 之外其余各元素的乘积就是左侧所有元素的乘积乘以右侧所有元素的乘积
+    //     for (int i = 0; i < length; i++) {
+    //         answer[i] = L[i] * R[i];
+    //     }
+
+    //     return answer;
+    // }
 }
 // @lc code=end
+
+// Accepted
+// 18/18 cases passed (1 ms)
+// Your runtime beats 100 % of java submissions
+// Your memory usage beats 11.76 % of java submissions (48.6 MB)
 
 // Accepted
 // 18/18 cases passed (1 ms)
